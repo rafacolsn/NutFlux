5c6472c178a2ddf577c697c562016d7e96b515b5
rafacolsn <raphcolson@gmail.com>
2018-12-14 16:05:00 +0100

    migrations

--- database/migrations/2018_12_14_144032_create_users_table.php
+++ database/migrations/2018_12_14_144032_create_users_table.php
@@ -15,11 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('avatar');
+            $table->unsignedInteger('account_id');
+            $table->foreign('account_id')->references('id')->on('accounts');
             $table->timestamps();
         });
     }
