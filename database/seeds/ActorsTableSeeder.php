7a8817759d47ec5714e6e068a3684c056b78bc58
TanguyScholtes <soultaker720@hotmail.com>
2018-12-18 11:13:08 +0100

    completed seeders with movies chosen by Marie and me

--- database/seeds/ActorsTableSeeder.php
+++ database/seeds/ActorsTableSeeder.php
@@ -26,7 +26,102 @@ class ActorsTableSeeder extends Seeder
                 'firstname' => 'Serge',
                 'lastname' => 'Hazanavicius',
                 'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Serge-Hazanavicius-Il-y-a-longtemps.JPG/220px-Serge-Hazanavicius-Il-y-a-longtemps.JPG'
-            ]
+            ],
+            [
+                'firstname' => 'Marion',
+                'lastname' => 'Cotillard',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Marion_Cotillard_C%C3%A9sar_2018.jpg/220px-Marion_Cotillard_C%C3%A9sar_2018.jpg'
+            ],
+            [
+                'firstname' => 'Jean-Pierre',
+                'lastname' => 'Martins',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Jean-Pierre_Martins.jpg/220px-Jean-Pierre_Martins.jpg'
+            ],
+            [
+                'firstname' => 'Gérard',
+                'lastname' => 'Depardieu',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/G%C3%A9rard_Depardieu_avp_2016_3.jpg/220px-G%C3%A9rard_Depardieu_avp_2016_3.jpg'
+            ],
+            [
+                'firstname' => 'Fabrice',
+                'lastname' => 'Luchini',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Fabrice_Luchini_Cannes_2016.jpg/220px-Fabrice_Luchini_Cannes_2016.jpg'
+            ],
+            [
+                'firstname' => 'Sandrine',
+                'lastname' => 'Kiberlain',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Sandrine_Kiberlain_Deauville_2018.jpg/220px-Sandrine_Kiberlain_Deauville_2018.jpg'
+            ],
+            [
+                'firstname' => 'Catherine',
+                'lastname' => 'Frot',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/MJK34645_Catherine_Frot_%28Sage_Femme%2C_Berlinale_2017%29.jpg/220px-MJK34645_Catherine_Frot_%28Sage_Femme%2C_Berlinale_2017%29.jpg'
+            ],
+            [
+                'firstname' => 'Déborah',
+                'lastname' => 'François',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Deborah_Fran%C3%A7ois_Cannes_2018.jpg/220px-Deborah_Fran%C3%A7ois_Cannes_2018.jpg'
+            ],
+            [
+                'firstname' => 'Emmanuelle',
+                'lastname' => 'Bercot',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Emmanuelle_Bercot_au_Festival_des_Busters_2017_%28cropped%29.jpg/220px-Emmanuelle_Bercot_au_Festival_des_Busters_2017_%28cropped%29.jpg'
+            ],
+            [
+                'firstname' => 'Vincent',
+                'lastname' => 'Cassel',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Vincent_Cassel_Cannes_2018.jpg/220px-Vincent_Cassel_Cannes_2018.jpg'
+            ],
+            [
+                'firstname' => 'François',
+                'lastname' => 'Cluzet',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Fran%C3%A7ois_Cluzet_C%C3%A9sars_2014.jpg/220px-Fran%C3%A7ois_Cluzet_C%C3%A9sars_2014.jpg'
+            ],
+            [
+                'firstname' => 'Lola',
+                'lastname' => 'Le Lann',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lola_Le_Lann_Cabourg_2015.jpg/220px-Lola_Le_Lann_Cabourg_2015.jpg'
+            ],
+            [
+                'firstname' => 'Elijah',
+                'lastname' => 'Wood',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Elijah_Wood_%2836103089852%29_%28cropped%29.jpg/240px-Elijah_Wood_%2836103089852%29_%28cropped%29.jpg'
+            ],
+            [
+                'firstname' => 'Ian',
+                'lastname' => 'McKellen',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/SDCC13_-_Ian_McKellen.jpg/240px-SDCC13_-_Ian_McKellen.jpg'
+            ],
+            [
+                'firstname' => 'Viggo',
+                'lastname' => 'Mortensen',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Viggo_Mortensen_Cannes_2014.jpg/220px-Viggo_Mortensen_Cannes_2014.jpg'
+            ],
+            [
+                'firstname' => 'Sean',
+                'lastname' => 'Astin',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Sean_Astin_by_Gage_Skidmore.jpg/220px-Sean_Astin_by_Gage_Skidmore.jpg'
+            ],
+            [
+                'firstname' => 'Orlando',
+                'lastname' => 'Bloom',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Orlando_Bloom_Cannes_2013.jpg/220px-Orlando_Bloom_Cannes_2013.jpg'
+            ],
+            [
+                'firstname' => 'Mark',
+                'lastname' => 'Hamill',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Mark_Hamill_%282017%29.jpg/220px-Mark_Hamill_%282017%29.jpg'
+            ],
+            [
+                'firstname' => 'Harrison',
+                'lastname' => 'Ford',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Harrison_Ford_by_Gage_Skidmore_2.jpg/250px-Harrison_Ford_by_Gage_Skidmore_2.jpg'
+            ],
+            [
+                'firstname' => 'Carrie',
+                'lastname' => 'Fisher',
+                'picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Carrie_Fisher_2013.jpg/250px-Carrie_Fisher_2013.jpg'
+            ],
         ] );
     }
 }
--- database/seeds/MediasTableSeeder.php
+++ database/seeds/MediasTableSeeder.php
@@ -13,15 +13,125 @@ class MediasTableSeeder extends Seeder
     {
         DB::table( 'medias' ) -> insert( [
             [
-                'title' => 'Il y a longtemps que je t\'aime',
-                'summary' => 'Deux sœurs, Juliette et Léa, se retrouvent après que Juliette a passé quinze années en prison. Léa, désormais mariée et avec deux enfants, l\'accueille dans sa maison à Nancy.',
+                'title' => 'I\'ve Loved You So Long',
+                'summary' => 'A woman struggles to interact with her family and find her place in society after spending fifteen years in prison.',
                 'year' => 2008,
-                'trailer' => 'igLstFcgk5g',
+                'trailer' => 'O6KYXAf6MXs',
                 'is_serie' => false,
                 'poster' => 'http://images.fan-de-cinema.com/affiches/large/38/58211.jpg',
                 'director' => 'Philippe Claudel',
                 'producer' => 'Yves Marmion'
-            ]
+            ],
+            [
+                'title' => 'La Vie En Rose',
+                'summary' => 'Biopic of the iconic French singer Édith Piaf. Raised by her grandmother in a brothel, she was discovered while singing on a street corner at the age of 19. Despite her success, Piaf\'s life was filled with tragedy.',
+                'year' => 2007,
+                'trailer' => 'uzEJ7NV_g98',
+                'is_serie' => false,
+                'poster' => 'http://fr.web.img2.acsta.net/r_1920_1080/medias/nmedia/18/60/15/65/18700879.jpg',
+                'director' => 'Olivier Dahan',
+                'producer' => 'Legende Films'
+            ],
+            [
+                'title' => 'Nothing about Robert',
+                'summary' => "Didier never knew that a wrong film review of a film which he did not bother to watch would land him in numerous troubles.",
+                'year' => 1999,
+                'trailer' => '_HUn8hQ_RsQ',
+                'is_serie' => false,
+                'poster' => 'http://fr.web.img4.acsta.net/r_1920_1080/medias/03/43/55/034355_af.jpg',
+                'director' => 'Pascal Bonitzer',
+                'producer' => 'Catherine Chouridis'
+            ],
+            [
+                'title' => 'The Page Turner',
+                'summary' => "After failing the audition to a prestigious music school because of the rude behavior of one of the testers, a girl takes revenge by gaining the trust of the same woman some years later and becoming indispensable for her.",
+                'year' => 2006,
+                'trailer' => 'fANWrJPhqWw',
+                'is_serie' => false,
+                'poster' => 'http://fr.web.img6.acsta.net/r_1920_1080/medias/nmedia/18/36/35/31/18649138.jpg',
+                'director' => 'Denis Dercourt',
+                'producer' => 'France 3 Cinéma'
+            ],
+            [
+                'title' => 'My king',
+                'summary' => "Tony is admitted to a rehabilitation center after a serious skiing accident. Dependent of medical staff and painkillers, she takes the time to remember the tumultuous love story she lived with Georgio.",
+                'year' => 2015,
+                'trailer' => 'wVkn58_wiM4',
+                'is_serie' => false,
+                'poster' => 'https://m.media-amazon.com/images/M/MV5BMjEzOTI2NzQ1Ml5BMl5BanBnXkFtZTgwNjI0NjI4OTE@._V1_SY1000_CR0,0,671,1000_AL_.jpg',
+                'director' => 'Maïwenn',
+                'producer' => 'Alain Attal'
+            ],
+            [
+                'title' => 'One Wild Moment',
+                'summary' => "Two friends bring their daughters with them on a beach vacation and find themselves in an awkward situation. A remake of the 1977 film \"Un moment d'égarement\".",
+                'year' => 2015,
+                'trailer' => '7TtfBZNfvpk',
+                'is_serie' => false,
+                'poster' => 'http://fr.web.img6.acsta.net/r_1920_1080/pictures/15/06/01/12/29/207428.jpg',
+                'director' => 'Jean-François Richet',
+                'producer' => 'La Petite Reine'
+            ],
+            [
+                'title' => 'The Lord of the Rings: The Fellowship of the Ring',
+                'summary' => "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
+                'year' => 2001,
+                'trailer' => 'V75dMMIW2B4',
+                'is_serie' => false,
+                'poster' => 'https://upload.wikimedia.org/wikipedia/en/9/9d/The_Lord_of_the_Rings_The_Fellowship_of_the_Ring_%282001%29_theatrical_poster.jpg',
+                'director' => 'Peter Jackson',
+                'producer' => 'WingNut Films'
+            ],
+            [
+                'title' => 'The Lord of the Rings: The Two Towers',
+                'summary' => "While Frodo and Sam edge closer to Mordor with the help of the shifty Gollum, the divided fellowship makes a stand against Sauron's new ally, Saruman, and his hordes of Isengard.",
+                'year' => 2002,
+                'trailer' => 'hYcw5ksV8YQ',
+                'is_serie' => false,
+                'poster' => 'https://upload.wikimedia.org/wikipedia/en/a/ad/Lord_of_the_Rings_-_The_Two_Towers.jpg',
+                'director' => 'Peter Jackson',
+                'producer' => 'WingNut Films'
+            ],
+            [
+                'title' => 'The Lord of the Rings: The Return of the King',
+                'summary' => "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
+                'year' => 2003,
+                'trailer' => 'r5X-hFf6Bwo',
+                'is_serie' => false,
+                'poster' => 'https://upload.wikimedia.org/wikipedia/en/9/9d/Lord_of_the_Rings_-_The_Return_of_the_King.jpg',
+                'director' => 'Peter Jackson',
+                'producer' => 'WingNut Films'
+            ],
+            [
+                'title' => 'Star Wars',
+                'summary' => "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
+                'year' => 1977,
+                'trailer' => 'vZ734NWnAHA',
+                'is_serie' => false,
+                'poster' => 'https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,643,1000_AL_.jpg',
+                'director' => 'George Lucas',
+                'producer' => 'Gary Kurtz'
+            ],
+            [
+                'title' => 'The Empire Strikes Back',
+                'summary' => "After the rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda, while his friends are pursued by Darth Vader.",
+                'year' => 1980,
+                'trailer' => 'JNwNXF9Y6kY',
+                'is_serie' => false,
+                'poster' => 'https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,641,1000_AL_.jpg',
+                'director' => 'Irvin Kershner',
+                'producer' => 'Gary Kurtz'
+            ],
+            [
+                'title' => 'Return of the Jedi',
+                'summary' => "After a daring mission to rescue Han Solo from Jabba the Hutt, the rebels dispatch to Endor to destroy a more powerful Death Star. Meanwhile, Luke struggles to help Darth Vader back from the dark side without falling into the Emperor's trap.",
+                'year' => 1983,
+                'trailer' => 'XgB4gaY2dWE',
+                'is_serie' => false,
+                'poster' => 'https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY999_CR0,0,644,999_AL_.jpg',
+                'director' => 'Richard Marquand',
+                'producer' => 'George Lucas'
+            ],
         ] );
     }
 }
