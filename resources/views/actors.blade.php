fe52a5b1f853c86002270b88b661b77597a183f0
rafacolsn <raphcolson@gmail.com>
2018-12-18 16:57:10 +0100

    show actor name n pic

---
+++ resources/views/actors.blade.php
@@ -0,0 +1,2 @@
+<p>{{ $actor->firstname }} {{ $actor->lastname }}</p>
+<img style="border-radius:50%" src="{{$actor->picture}}" alt="">
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::resource( 'accounts', 'AccountController' );
+// Route::get( 'accounts/{account}/delete', 'AccountController@delete' ) -> name( 'accounts.delete' );
+
+// Route::resource( 'actors', 'ActorController' );
+// Route::get( 'actors/{actor}/delete', 'ActorController@delete' ) -> name( 'actors.delete' );
+
+Route::get('actors/{id}', 'ActorController@show');
