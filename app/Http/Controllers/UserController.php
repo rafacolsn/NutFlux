19104bd0adaec0ce50204964e6d2c2b799d9d8bf
TanguyScholtes <soultaker720@hotmail.com>
2019-01-11 15:01:08 +0100

    added users restriction of 3 in users controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,7 +30,16 @@ class UserController extends Controller
      */
     public function create()
     {
-        return view('users.create');
+        //get all users of logged in account
+        $accountUsers = User::where( 'account_id', Auth::id() ) -> get();
+        if ( !isset( $accountUsers ) || count( $accountUsers ) > 2 ) {
+            //if account does not exists or has more than 2 accounts
+            //redirect to users index
+            return redirect() -> route( 'users.index' );
+        }
+        //else (if account exists and has 2 users or less)
+        //display users create form
+        return view( 'users.create' );
     }
 
     /**
@@ -41,13 +50,22 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
+        $id = Auth::id();
+        //get all users of logged in account
+        $accountUsers = User::where( 'account_id', $id ) -> get();
+        if ( !isset( $accountUsers ) || count( $accountUsers ) > 2 ) {
+            //if account does not exists or has more than 2 accounts
+            //redirect to users index
+            return redirect() -> route( 'users.index' );
+        }
+        //else (if account exists and has 2 users or less)
+
         $this->validate($request, [
             'firstname' => 'required',
             'lastname' => 'required',
             'avatar' => 'required',
-            
         ]);
-        $id = Auth::id();
+
         $user = new User([
             'firstname' => $request->get('firstname'),
             'lastname'=> $request->get('lastname'),
@@ -67,7 +85,7 @@ class UserController extends Controller
     public function show($id)
     {
         $usersObj = User::find($id);
-        
+
         return view('users.show', compact('usersObj'));
     }
 
@@ -97,13 +115,13 @@ class UserController extends Controller
             'userLastname' => 'required',
             // 'userAvatar' => 'required',
         ]);
-        
+
         $user = User::find($id);
         $user->firstname = $request->get('userFirstname');
         $user->lastname = $request->get('userLastname');
         $user->avatar = $request->get('userAvatar');
         $user->save();
-  
+
         return redirect()->route('users.index')->with('success','User updated successfully');
     }
 
@@ -120,13 +138,13 @@ class UserController extends Controller
         return redirect()->route('users.index')->with('success', $user->firstname.' '.$user->lastname.' has been deleted');
     }
 
-    public function login(Request $request, $id) 
+    public function login(Request $request, $id)
     {
         $user = User::find($id);
         $request->session()->put('user', $user);
-        
+
         return redirect()->route('choices.index');
-        
+
     }
 
     public function __construct()
