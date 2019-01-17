d22e58d222ba5835f361aaec9a1cbd31ee3d5822
TanguyScholtes <soultaker720@hotmail.com>
2019-01-17 11:00:40 +0100

    debugging post routes on deployment (in progress)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,5 +5,20 @@
 @stop
 
 @section( 'content' )
-    <accounts-login csrf="{{csrf_token()}}"></accounts-login>
+    <form action="/login/" method="post">
+        @csrf
+        <p class="input-wrapper">
+            <label class="input-label" for="email">Email:</label>
+            <input class="input-field" type="email" name="email" placeholder="Your email address">
+        </p>
+
+        <p class="input-wrapper">
+            <label class="input-label" for="password">Password:</label>
+            <input class="input-field" type="password" name="password" placeholder="Type your password">
+        </p>
+        <div class="container-button">
+            <button class="btn" type="submit">Login</button>
+        </div>
+    </form>
+    <!-- <accounts-login csrf="{{csrf_token()}}"></accounts-login> -->
 @stop
