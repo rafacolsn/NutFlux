f4878f3da014798705956b32579c00dadb704a9d
TanguyScholtes <soultaker720@hotmail.com>
2019-01-17 10:32:44 +0100

    debugging post routes on deployment (in progress)

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -43,7 +43,6 @@ class AccountController extends Controller
      */
     public function store(Request $request)
     {
-        dd( 'this is register controller. You made it this far. Yay.' );
         $this -> validate( $request, [
             'email' => 'required',
             'password' => 'required',
